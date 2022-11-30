faa9625ecb136a1bbe3bf9a79787230608a8ddfa
DIFA <42770277+green404@users.noreply.github.com>
2022-11-30 17:57:05 +0700

    Create UMIWAB.js

---
+++ UMIWAB.js
@@ -0,0 +1,14 @@
+{
+	"version": "18.0",
+	"download": "https://difarosyidin.blogspot.com",
+	"update": [
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/green404/UMIWAB/main/changelog-B-en.html"
+		},
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/changelog-B-es.html"
+		}
+	]
+}
