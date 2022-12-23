725128315b4f97ce5f0b8df14085fed1e4bff608
DIFA <42770277+green404@users.noreply.github.com>
2022-12-24 06:46:34 +0700

    Update UMIWAB.js

--- UMIWAB.js
+++ UMIWAB.js
@@ -1,14 +1,14 @@
 {
-	"version": "19.0",
+	"version": "18.0",
 	"download": "https://difarosyidin.blogspot.com",
 	"update": [
 		{
 			"languaje": "en",
-			"changelog": "https://raw.githubusercontent.com/green404/UMIWAB/main/tes.html"
+			"changelog": "https://raw.githubusercontent.com/green404/UMIWAB/main/Changelog.html"
 		},
 		{
 			"languaje": "es",
-			"changelog": "https://raw.githubusercontent.com/green404/UMIWAB/main/tes.html"
+			"changelog": "https://raw.githubusercontent.com/green404/UMIWAB/main/Changelog.html"
 		}
 	]
 }
